041bd4b99985427d9b62f69c78dd4251f7cf2870
zobenali <zohrabenali98@yahoo.fr>
2025-05-03 15:24:16 +0100

    Add Data Processing and Model definition code files

---
+++ model.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+from data_processing import get_num_classes
+
+
+def create_model(model_name, dataframe):
+    num_classes = get_num_classes(dataframe)
+    if model_name == 'resnet18':
+        model = models.resnet18(weights = models.ResNet18_Weights) # Use the latest weights available
+        
+    elif model_name == 'resnet50':
+        model = models.resnet50(weights=models.ResNet50_Weights)
+        model.fc = torch.nn.Linear(model.fc.in_features, num_classes)
+    elif model_name == 'densenet121':
+        model = models.densenet121(weights=models.DenseNet121_Weights)
+        model.classifier[1] = torch.nn.Linear(model.classifier[1].in_features, num_classes)
+    else:
+        raise ValueError(f"Model {model_name} not supported")
+    
+    n_features = model.fc.in_features if hasattr(model, 'fc') else model.classifier[1].in_features
+    model.fc = nn.Sequential(
+                                nn.Flatten(),
+                                nn.Linear(n_features, 256),
+                                nn.ReLU(),
+                                #nn.Dropout(p=0.5),
+                                nn.Linear(256, num_classes)
+                            )
+    return model
---
+++ src/data_processing.py
@@ -0,0 +1,144 @@
+import os
+import glob
+import pandas as pd
+import numpy as np
+import torch
+from torchvision import transforms
+from PIL import Image
+
+
+normalise_resize = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # normalisation
+])
+
+def format_image(img_file):
+	"""
+	This function reads and formats an image so that it can be fed to the ResNET network
+
+	Parameters
+	----------
+	img_file : image file name
+
+	Returns
+	-------
+	img_out_model : the correctly formatted image 
+	img : the image as read by the load_img function of keras.preprocessing.image
+	"""
+	# Read image
+	img = Image.open(img_file)
+	img_tensor = normalise_resize(img).unsqueeze(axis=0)
+	img_np = np.array(img)
+
+	return img_tensor, img_np
+
+def unformat_image(img_in):
+	"""
+	This function inverts the preprocessing applied to images for use in the VGG16/inceptionv3 network
+
+	Parameters
+	----------
+	img_file : formatted image of shape (batch_size,m,n,3)
+
+	Returns
+	-------
+	img_out : a m-by-n-by-3 array, representing an image that can be written to an image file
+	"""
+
+	img_in = np.transpose(img_in.detach().numpy().squeeze(),[1,2,0])
+	# invert the mean and standard deviation
+	mean = np.array([0.485, 0.456, 0.406]).reshape([1, 1, 3])
+	std = np.array([0.229, 0.224, 0.225]).reshape([1, 1, 3])
+	img_out = std * img_in + mean
+	img_out *= 255
+	img_out = np.uint8(np.clip(img_out, 0, 255))
+
+	return img_out
+
+def create_dataframe(working_directory):
+    painters = [name for name in os.listdir(working_directory) if os.path.isdir(os.path.join(working_directory, name))] #liste des peintres
+
+    images_by_painter = {} # Dictionnary with painter name as key and list of images as value
+
+    image_names = []
+    painter_names = []
+    painter_names_full = []
+    bdates = []
+
+    for painter in painters:
+        painter_path = os.path.join(working_directory, painter)
+        image_paths = glob.glob(os.path.join(painter_path, '*'))
+        images_by_painter[painter] = image_paths
+        for image_path in image_paths:
+            image_name = os.path.basename(image_path)
+            image_names.append(image_name)
+            painter_names.append(painter.split(' - ')[0]) # Only keep the name of the painter, not the birthdate
+            painter_names_full.append(painter)
+            bdates.append(int(painter.split('_')[1])) # Keep the birthdate as an integer
+
+    df = pd.DataFrame({
+        'image_name': image_names,
+        'painter': painter_names,
+        'date' : bdates
+    })
+
+    return df,image_names, painter_names, painter_names_full, images_by_painter
+
+def create_tensor(df, image_names, painter_names_full, Working_directory):
+    """
+    This function creates a tensor of images and a tensor of labels from the dataframe
+    and the image names
+    Parameters
+    ---------- 
+    df : pandas dataframe
+        dataframe containing the image names and the labels 
+    image_names : list of str
+        list of image names
+    painter_names_full : list of str
+        list of painter names
+    Working_directory : str
+        path to the working directory  
+    
+    Returns
+    ------- 
+    X_tensor : torch.Tensor 
+        tensor of images of shape (n_images, 3, 224, 224)
+    Y_tensor : torch.Tensor
+        tensor of labels of shape (n_images,)   
+    
+    """
+    X = [] #Image Tensor
+    Y = [] #labels
+
+    label_to_int = {label: idx for idx, label in enumerate(df['painter'].unique())} #change painter as
+
+    for n in range(len(image_names)):
+        img_path = os.path.join(Working_directory, painter_names_full[n], df.iloc[n]['image_name'])
+        img_tensor, _ = format_image(img_path)
+
+        X.append(img_tensor)
+        Y.append(label_to_int[df.iloc[n]['painter']])
+    
+    X_tensor = torch.cat(X, dim=0)
+    Y_tensor = torch.tensor(Y, dtype=torch.long)
+
+    return X_tensor, Y_tensor, label_to_int
+
+def get_num_classes(df):
+    """
+    This function returns the number of classes in the dataframe
+    Parameters
+    ---------- 
+    df : pandas dataframe
+        dataframe containing the image names and the labels 
+    
+    Returns
+    ------- 
+    num_classes : int 
+        number of classes in the dataframe
+    
+    """
+    num_classes = len(df['painter'].unique())
+
+    return num_classes
